c34a09b54166e51209adb5217fe4a3ccb4303ef2
arindam1997007 <arindamchy00@gmail.com>
2024-06-16 04:13:39 +0530

    Changes
    - Increased color opacity for FadedHeader
    - Changed content of Footer component

--- components/ui/fadedHeader.tsx
+++ components/ui/fadedHeader.tsx
@@ -11,7 +11,7 @@ export const FadedHeader = ({
 		<div className={cn("relative text-[14vw] min-h-[0.6lh]", className)}>
 			<h2
 				className={cn(
-					"absolute -z-10 bg-gradient-to-b from-accent/35 to-background to-75% bg-clip-text text-transparent text-center w-full"
+					"absolute -z-10 bg-gradient-to-b from-accent/55 to-background to-80% bg-clip-text text-transparent text-center w-full"
 				)}
 			>
 				{text}
